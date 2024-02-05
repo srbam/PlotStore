f1aa3aa1471a1987bfebc5a3b74f3ac7183e07d5
srbam <srbam02@hotmail.com>
2024-02-04 21:26:05 -0300

    temp: store page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './store/store.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent }from './about/about.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'store', component: StoreComponent }
+    { path: 'store', component: StoreComponent },
+    { path: 'about', component: AboutComponent }
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
